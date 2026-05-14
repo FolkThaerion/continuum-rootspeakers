81962e494b55a5bded4fa53eadf4c8d492e2bfe2
John Franchi <ContinuumsBreath@proton.me>
2026-05-14 10:57:05 -0400

    Fix secured cron syntax

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -9,18 +9,19 @@ export async function GET(request: Request) {
   }
 
   const baseUrl =
-  process.env.NEXT_PUBLIC_SITE_URL ||
-  "http://localhost:3000";
+    process.env.NEXT_PUBLIC_SITE_URL ||
+    "http://localhost:3000";
 
-const response = await fetch(`${baseUrl}/api/events`, {
-  method: "POST",
-});
+  const response = await fetch(`${baseUrl}/api/events`, {
+    method: "POST",
+  });
 
-const data = await response.json();
+  const data = await response.json();
 
-return Response.json({
-  success: true,
-  message: "Scheduled Continuum event triggered.",
-  eventResult: data,
-});
+  return Response.json({
+    success: true,
+    message: "Scheduled Continuum event triggered.",
+    eventResult: data,
+  });
+}
  
